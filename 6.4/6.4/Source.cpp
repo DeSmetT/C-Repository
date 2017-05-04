2c3eec59002a2e5a7295f88f35f5885def680880
DeSmetT <td137139@students.davenport.k12.ia.us>
2017-05-04 11:06:10 -0500

    Update

---
+++ 6.4/6.4/Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+	
+
+	int arrayname[] = { 23, 6, 47, 35, 2, 14 };
+	int a, sum =0;
+	for (a = 0; a < 47; ++a)
+	{
+
+		sum = (23 + 6 + 47 + 35 + 2 + 14) / 6;
+	}
+
+	cout << sum << endl;
+
+	return 0;
+}
